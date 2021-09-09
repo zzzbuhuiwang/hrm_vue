d1190ff53d8418ff6a9181f63e142b099803f461
zzzbuhuiwang <zzzbuhuiwang@163.com>
2021-09-09 15:33:25 +0800

    hrm_vue 前端脚手架初始化

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"138000138000",
+            username:"张三",
+            company:"xxx公司",
+            email:"xxx@xxx.xx",
+        }
+    }
+}
